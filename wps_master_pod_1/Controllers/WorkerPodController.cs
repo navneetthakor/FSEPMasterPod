490972a748940f2ec5a91577c7cdfe6cedd32ef2
navneetthakor <tnavneet8628@gmail.com>
2025-04-29 21:55:51 +0530

    workerPodController modified

--- wps_master_pod_1/Controllers/WorkerPodController.cs
+++ wps_master_pod_1/Controllers/WorkerPodController.cs
@@ -20,27 +20,53 @@ namespace wps_master_pod_1.Controllers
         }
 
         [HttpGet]
-        [Route("HeartBeat")]
-        public Response HeartBeat([FromServices] IRecurringJobManager recurringJobManager ,string worker_id)
+        [Route("WorkerHeartBeat")]
+        public Response WorkerHeartBeat([FromServices] IRecurringJobManager recurringJobManager ,string worker_id)
         {
             try
             {
-                //--- older code --- 
-                //try to update the worker pod status in Database first 
-                //Response response = _dbConnection.AcknowledgeServer(worker_id);
-
-                //if(response.IsError)
-                //{
-                //    //if error occured then push this to stackStore 
-                //    //so that it can be processed later
-                //    stackStore.AddWorker(worker_id);
-                //}
-                //return response;
-
-
-                //--- newer code --- 
                 //Update hangfire entry 
                 string? message = "Hello admin! None of the worker POD had reported in last time span";
+                recurringJobManager.AddOrUpdate("Checking-health", () => MyKafkaProducer.NotifyAdmin(message), "*/3 * * * *");
+
+                // return response
+                return new Response { Data = null, ErrorMessage = "successfully reported", IsError = false };
+
+
+            }
+            catch (Exception ex)
+            {
+                return new Response { Data = null, ErrorMessage = ex.Message, IsError = true };
+            }
+        }
+        [HttpGet]
+        [Route("UserMngHeartBeat")]
+        public Response UserMngHeartBeat([FromServices] IRecurringJobManager recurringJobManager ,string worker_id)
+        {
+            try
+            {
+                //Update hangfire entry 
+                string? message = "Hello admin! None of the User-Management POD had reported in last time span";
+                recurringJobManager.AddOrUpdate("Checking-health", () => MyKafkaProducer.NotifyAdmin(message), "*/3 * * * *");
+
+                // return response
+                return new Response { Data = null, ErrorMessage = "successfully reported", IsError = false };
+
+
+            }
+            catch (Exception ex)
+            {
+                return new Response { Data = null, ErrorMessage = ex.Message, IsError = true };
+            }
+        }
+        [HttpGet]
+        [Route("AlertingSysHeartBeat")]
+        public Response AlertingSysHeartBeat([FromServices] IRecurringJobManager recurringJobManager ,string worker_id)
+        {
+            try
+            {
+                //Update hangfire entry 
+                string? message = "Hello admin! None of the Alerting-System POD had reported in last time span";
                 recurringJobManager.AddOrUpdate("Checking-health", () => MyKafkaProducer.NotifyAdmin(message), "*/3 * * * *");
 
                 // return response
